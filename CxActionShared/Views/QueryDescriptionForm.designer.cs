5e7319a7b4fa6ebb48b4d62ac64d765255ba1772
Rahul Pidde <Rahul.Pidde@checkmarx.com>
2023-04-04 09:38:56 +0530

    Changes done related to Show vuln description in embedded browser

---
+++ CxActionShared/Views/QueryDescriptionForm.designer.cs
@@ -0,0 +1,81 @@
+﻿namespace CxViewerAction.Views
+{
+    partial class QueryDescriptionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing && (components != null))
+        //    {
+        //        components.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QueryDescriptionForm));
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.pContainer = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(3, 2);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(1190, 26);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Visible = false;
+            // 
+            // pContainer
+            // 
+            this.pContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pContainer.Location = new System.Drawing.Point(3, 2);
+            this.pContainer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pContainer.Name = "pContainer";
+            this.pContainer.Size = new System.Drawing.Size(1727, 934);
+            this.pContainer.TabIndex = 1;
+            // 
+            // QueryDescriptionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1728, 937);
+            this.Controls.Add(this.pContainer);
+            this.Controls.Add(this.txtUrl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "QueryDescriptionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Query Description";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.QueryDescription_FormClosed);
+            this.Load += new System.EventHandler(this.QueryDescriptionForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Panel pContainer;
+    }
+}
